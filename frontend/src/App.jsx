9a4f1b8cc70fb1e88a42c2a138791c958e31b6dd
Zainab <pk.x3311@gmail.com>
2024-07-14 19:48:20 +0300

    structured frontend folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <main className="bg-primary text-tertiary
+     ">
+      <Header />
+    </main>
+  )
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+
+
+const Header = () => {
+    return (
+        <header>
+
+            <div>
+                {/* logo */}
+                <Link></Link>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
