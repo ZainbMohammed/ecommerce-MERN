cdc0d878683a63dfcd0fc76072e7673ac6dd2b5e
Zainab <pk.x3311@gmail.com>
2024-07-21 05:23:58 +0300

    product detailes page => Product description  details done

---
+++ frontend/src/components/ProductDescription.jsx
@@ -0,0 +1,23 @@
+
+const ProductDescription = () => {
+  return <>
+  
+  <div className="mt-20">
+    <div className='flex gap-3 mb-4'>
+        <button className='btn_dark_rounded !rounded-none !text-xs !py-[6px] w-36'>Description</button>
+        <button className='btn_dark_outline !rounded-none !text-xs !py-[6px] w-36'>Care Guide</button>
+        <button className='btn_dark_outline !rounded-none !text-xs !py-[6px] w-36'>Size Guide</button>
+    </div>
+    <div className='flex flex-col pb-16'>
+        <p className='text-sm'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, molestias? Doloribus laboriosam quasi dignissimos inventore pariatur illum aperiam laborum neque, facilis, commodi exercitationem dicta accusamus. Quia blanditiis consectetur velit in excepturi, ab similique cupiditate magnam repudiandae mollitia voluptatem assumenda deserunt corrupti, odio voluptatum optio recusandae ad tempora eum molestias. Dolorem exercitationem quis aliquam architecto minima.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae doloremque nam adipisci necessitatibus numquam reiciendis saepe dolores eius, explicabo, sint quaerat temporibus in rem porro quidem, et earum?
+        </p>
+    </div>
+  </div>
+  </>
+}
+
+export default ProductDescription
