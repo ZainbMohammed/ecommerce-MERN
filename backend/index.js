548c393de6fb9d3892d17f141a54e05339f6a242
Zainab <pk.x3311@gmail.com>
2024-07-23 20:28:06 +0300

    connect successful with AtlasMongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const poet = 4000;
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -9,3 +9,17 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+
+try {
+     mongoose.connect(process.env.MONGO_URL);
+    console.log("Mongoose is connected successful");
+  } catch (e) {
+    // connection faield
+    console.log("could not connect");
+  }
+app.get('/',(req,res)=> {
+    res.send('Hi From Express');
+})
+  app.listen(process.env.PORT,()=>{
+    console.log('app listing on port 3000');
+})
