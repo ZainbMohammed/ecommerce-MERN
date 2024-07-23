c85ffa0d719cec181ba1068429192686b6802b8c
Zainab <pk.x3311@gmail.com>
2024-07-24 00:57:28 +0300

    perform upload image endpoint done successful

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,99 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const path = require('path');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
 
 try {
-     mongoose.connect(process.env.MONGO_URL);
-    console.log("Mongoose is connected successful");
-  } catch (e) {
-    // connection faield
-    console.log("could not connect");
-  }
-app.get('/',(req,res)=> {
-    res.send('Hi From Express');
+  mongoose.connect(process.env.MONGO_URL);
+  console.log("Mongoose is connected successful");
+} catch (e) {
+  // connection faield
+  console.log("could not connect");
+}
+app.get("/", (req, res) => {
+  res.send("Hi From Express");
+});
+
+// image stoarge engin
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()} ${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// create upload endpoint for images
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${process.env.PORT}/images/${req.file.filename}`,
+  });
+});
+
+// schema for creation products
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_peice: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.post('/addProduct',async (req,res) => {
+
+  const product = new Product({
+    id: req.body.id,
+    name: req.body.name,
+    image: req.body.image,
+    image: req.body.image,
+    image: req.body.image,
+    image: req.body.image,
+
+  })
 })
-  app.listen(process.env.PORT,()=>{
-    console.log('app listing on port 3000');
-})
+
+app.listen(process.env.PORT, () => {
+  console.log("app listing on port 3000");
+});
