cd3d92857a61ab162d69ad9a011b5d350ad93651
Zainab <pk.x3311@gmail.com>
2024-07-29 02:14:33 +0300

    add product schema

--- backend/index.js
+++ backend/index.js
@@ -45,41 +45,41 @@ app.post("/upload", upload.single("product"), (req, res) => {
   });
 });
 
-// schema for creation products
-// const Product = mongoose.model("Product", {
-//   id: {
-//     type: Number,
-//     required: true,
-//   },
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   image: {
-//     type: String,
-//     required: true,
-//   },
-//   category: {
-//     type: String,
-//     required: true,
-//   },
-//   new_price: {
-//     type: Number,
-//     required: true,
-//   },
-//   old_peice: {
-//     type: Number,
-//     required: true,
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now(),
-//   },
-//   available: {
-//     type: Boolean,
-//     default: true,
-//   },
-// });
+schema for creation products
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_peice: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 app.post('/addProduct',async (req,res) => {
 
